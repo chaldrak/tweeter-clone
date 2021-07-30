78f880563eac3fcc95c9b406f879ca8cb56113a4
Chaldrak <chaldrakus@gmail.com>
2021-07-30 08:17:29 +0100

    list of posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,10 @@
 class PostsController < ApplicationController
+
+
   def index
+    @posts = Post.all.order(created_at: :desc)
+    @post = Post.new
   end
+
+  
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,3 @@
 class Post < ApplicationRecord
-    validates: content, length: {in: 1..140}
+    validates :content, length: {in: 1..140, message: " doit comporter entre 1 et 140 caractères"}
 end
